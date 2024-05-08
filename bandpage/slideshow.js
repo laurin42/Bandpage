f09d40de9d962732dd012c4023a28c8bafa71155
laurin42 <laurin.schmidt@yahoo.com>
2024-05-08 15:56:20 +0200

    slideshow update

---
+++ bandpage/slideshow.js
@@ -0,0 +1,26 @@
+const indikatoren = document.getElementsByClassName("indikator");
+indikatoren[0].classList.add("aktiv");
+const slide = document.getElementsByClassName("slide");
+slide[0].classList.add("aktiv");
+
+let aktuellerIndex = 0;
+
+function umschalten(anzahl) {
+    let neuerIndex = aktuellerIndex + anzahl;
+    if(neuerIndex < 0) {
+        neuerIndex = slide.length -1;
+    }
+    if(neuerIndex > slide.length -1) {
+        neuerIndex = 0;
+    }
+    springeZuEintrag(neuerIndex);
+}
+function springeZuEintrag(neuerIndex) {
+    indikatoren[aktuellerIndex].classList.remove("aktiv");
+    slide[aktuellerIndex].classList.remove("aktiv");
+
+    indikatoren[neuerIndex].classList.add("aktiv");
+    slide[neuerIndex].classList.add("aktiv");
+
+    aktuellerIndex = neuerIndex;
+}
