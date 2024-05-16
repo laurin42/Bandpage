bee23e75d9e93f484c6ced9c9db63ccd846bf755
laurin42 <laurin.schmidt@yahoo.com>
2024-05-16 17:24:58 +0200

    finish

--- server/app.js
+++ server/app.js
@@ -4,49 +4,36 @@ var cors = require('cors')
 const app = express();
 const port = 3000;
 const nodemailer = require('nodemailer')
-// Middleware zum Parsen von Anfragekörpern
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(cors())
 
-
 const transporter = nodemailer.createTransport({
-  host: "smtp.mail.yahoo.com",
-  port: 465,
-  secure: false, // Use `true` for port 465, `false` for all other ports
+  host: 'smtp-relay.brevo.com',
+  port: 587,
   auth: {
-    user: "vidal79@ethereal.email",
-    pass: "nF43GuTVFbkYQVnKth",
-  },
+      user: '74ded2001@smtp-brevo.com',
+      pass: 'NyKZgOH9QftsS0P7'
+  }
 });
-
 // POST-Route für die Verarbeitung von Formulardaten
 app.post('/submit-form', async (req, res) => {
   // Daten aus dem Anfragekörper abrufen
   const formData = req.body;
-
-
   // Hier kannst du die Daten verarbeiten, z.B. in einer Datenbank speichern
   console.log('Received form data:', formData);
-
+  transporter.sendMail({
+    from: 'laurin.schmidt420@gmail.com',
+    to: 'laurin.schmidt@yahoo.com',
+    subject: 'Message',
+    text: formData.name + formData.email + formData.message,
+}, (err, info) => {
+  console.log(err);
+    console.log(info.envelope);
+    console.log(info.messageId);
+});
   // Antworte dem Client mit einer Bestätigungsnachricht
   res.send('Form submitted successfully!');
 });
-app.get('/styles', async (req, res) => {
-  // Daten aus dem Anfragekörper abrufen
-
-  const data = {
-    fontSize: "5rem",
-    text: "Hallo wir sind!",
-    color: "green"
-  }
-
-  // Hier kannst du die Daten verarbeiten, z.B. in einer Datenbank speichern
-
-  // Antworte dem Client mit einer Bestätigungsnachricht
-  res.send(data);
-});
-
 // Server starten
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
